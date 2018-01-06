7d3bdfd487f24f813fb069d8854180341bff2328
Holger Woltersdorf <hw@hollo.me>
2018-01-06 18:51:59 +0100

    Adds base exceptions

---
+++ src/Application/Exceptions/LogicException.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+/**
+ * @author hollodotme
+ */
+
+namespace PHPUGDD\PHPDD\Website\Application\Exceptions;
+
+/**
+ * Class LogicException
+ * @package PHPUGDD\PHPDD\Website\Application\Exceptions
+ */
+class LogicException extends \LogicException
+{
+
+}
---
+++ src/Application/Exceptions/RuntimeException.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+/**
+ * @author hollodotme
+ */
+
+namespace PHPUGDD\PHPDD\Website\Application\Exceptions;
+
+/**
+ * Class RuntimeException
+ * @package PHPUGDD\PHPDD\Website\Application\Exceptions
+ */
+class RuntimeException extends \RuntimeException
+{
+
+}
