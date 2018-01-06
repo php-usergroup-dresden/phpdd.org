d6969b1708d0e10fae3feacaaeee2fd0b99907f2
Holger Woltersdorf <hw@hollo.me>
2018-01-06 18:53:55 +0100

    Adds application bootstrap script

---
+++ public/2018/index.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+/**
+ * @author hollodotme
+ */
+
+namespace PHPUGDD\PHPDD\Website;
+
+use IceHawk\IceHawk\IceHawk;
+use PHPUGDD\PHPDD\Website\Infrastructure\IceHawkConfig;
+use PHPUGDD\PHPDD\Website\Infrastructure\IceHawkDelegate;
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+$env      = new Env();
+$config   = new IceHawkConfig( $env );
+$delegate = new IceHawkDelegate( $env );
+$icehawk  = new IceHawk( $config, $delegate );
+
+$icehawk->init();
+$icehawk->handleRequest();
