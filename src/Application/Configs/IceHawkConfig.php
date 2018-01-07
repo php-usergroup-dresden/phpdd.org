24be3f3c874fb4b6e396a9f61f6c86dd14ec8d6b
Holger Woltersdorf <hw@hollo.me>
2018-01-07 16:12:06 +0100

    Moves icehawk config to application

--- src/Application/Configs/IceHawkConfig.php
+++ src/Application/Configs/IceHawkConfig.php
@@ -3,7 +3,7 @@
  * @author hollodotme
  */
 
-namespace PHPUGDD\PHPDD\Website\Infrastructure;
+namespace PHPUGDD\PHPDD\Website\Application\Configs;
 
 use IceHawk\IceHawk\Defaults\Traits\DefaultCookieProviding;
 use IceHawk\IceHawk\Defaults\Traits\DefaultEventSubscribing;
@@ -12,13 +12,16 @@ use IceHawk\IceHawk\Defaults\Traits\DefaultRequestInfoProviding;
 use IceHawk\IceHawk\Interfaces\ConfiguresIceHawk;
 use IceHawk\IceHawk\Interfaces\RespondsFinallyToReadRequest;
 use IceHawk\IceHawk\Interfaces\RespondsFinallyToWriteRequest;
+use IceHawk\IceHawk\Routing\Patterns\NamedRegExp;
+use IceHawk\IceHawk\Routing\ReadRoute;
+use IceHawk\IceHawk\Routing\WriteRoute;
 use PHPUGDD\PHPDD\Website\Application\FinalResponders\FinalReadResponder;
 use PHPUGDD\PHPDD\Website\Application\FinalResponders\FinalWriteResponder;
 use PHPUGDD\PHPDD\Website\Traits\InfrastructureInjecting;
 
 /**
  * Class IceHawkConfig
- * @package PHPUGDD\PHPDD\Website\Infrastructure
+ * @package PHPUGDD\PHPDD\Website\Application\Configs
  */
 final class IceHawkConfig implements ConfiguresIceHawk
 {
@@ -29,14 +32,30 @@ final class IceHawkConfig implements ConfiguresIceHawk
 	use DefaultRequestBypassing;
 	use DefaultRequestInfoProviding;
 
-	public function getReadRoutes()
+	public function getReadRoutes() : \Generator
 	{
-		return [];
+		$routes = require __DIR__ . '/../../../config/ReadRoutes.php';
+
+		foreach ( (array)$routes as $pattern => $handlerClass )
+		{
+			yield new ReadRoute(
+				new NamedRegExp( $pattern, 'i' ),
+				new $handlerClass( $this->getEnv() )
+			);
+		}
 	}
 
-	public function getWriteRoutes()
+	public function getWriteRoutes() : \Generator
 	{
-		return [];
+		$routes = require __DIR__ . '/../../../config/WriteRoutes.php';
+
+		foreach ( (array)$routes as $pattern => $handlerClass )
+		{
+			yield new WriteRoute(
+				new NamedRegExp( $pattern, 'i' ),
+				new $handlerClass( $this->getEnv() )
+			);
+		}
 	}
 
 	public function getFinalReadResponder() : RespondsFinallyToReadRequest
