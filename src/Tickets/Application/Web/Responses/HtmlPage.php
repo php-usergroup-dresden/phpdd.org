4c4abb043d4f40180642f6731b41228a0777f5c7
Holger Woltersdorf <hw@hollo.me>
2018-02-18 22:58:44 +0100

    Adds data from project config as twig variables

--- src/Tickets/Application/Web/Responses/HtmlPage.php
+++ src/Tickets/Application/Web/Responses/HtmlPage.php
@@ -21,7 +21,34 @@ final class HtmlPage
 		$templateRenderer = $this->getEnv()->getTemplateRenderer();
 
 		header( 'Content-Type: text/html; charset=utf-8', true, $httpCode );
-		echo $templateRenderer->renderWithData( $template, $data );
+		echo $templateRenderer->renderWithData( $template, $this->getMergedData( $data ) );
 		flush();
+	}
+
+	private function getMergedData( array $data ) : array
+	{
+		$projectConfig = $this->getDataFromProjectConfig();
+
+		$replacements = [];
+		foreach ( (array)$projectConfig['replacements'] as $key => $value )
+		{
+			$replacements[ str_replace( '@', '', $key ) ] = $value;
+		}
+
+		return array_merge(
+			[
+				'projectName' => $projectConfig['name'],
+				'baseUrl'     => $projectConfig['baseUrl'],
+			],
+			$replacements,
+			$data
+		);
+	}
+
+	private function getDataFromProjectConfig() : array
+	{
+		$configData = file_get_contents( __DIR__ . '/../../../../../Project2018.json' );
+
+		return json_decode( $configData, true );
 	}
 }
