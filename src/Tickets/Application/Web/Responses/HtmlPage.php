67ac97aa258fd6552eb87f88c610960b3a658cb0
Holger Woltersdorf <hw@hollo.me>
2018-02-19 21:59:34 +0100

    Adds project/page configs for usage of origin theme

--- src/Tickets/Application/Web/Responses/HtmlPage.php
+++ src/Tickets/Application/Web/Responses/HtmlPage.php
@@ -6,6 +6,7 @@
 namespace PHPUGDD\PHPDD\Website\Tickets\Application\Web\Responses;
 
 use IceHawk\IceHawk\Constants\HttpCode;
+use PHPUGDD\PHPDD\Website\Tickets\Application\Configs\ProjectConfig;
 use PHPUGDD\PHPDD\Website\Tickets\Traits\InfrastructureInjecting;
 
 /**
@@ -16,39 +17,72 @@ final class HtmlPage
 {
 	use InfrastructureInjecting;
 
+	/** @var ProjectConfig */
+	private $projectConfig;
+
+	/**
+	 * @param string $template
+	 * @param array  $data
+	 * @param int    $httpCode
+	 *
+	 * @throws \PHPUGDD\PHPDD\Website\Tickets\Application\Exceptions\RuntimeException
+	 */
 	public function respond( string $template, array $data, int $httpCode = HttpCode::OK ) : void
 	{
 		$templateRenderer = $this->getEnv()->getTemplateRenderer();
 
 		header( 'Content-Type: text/html; charset=utf-8', true, $httpCode );
-		echo $templateRenderer->renderWithData( $template, $this->getMergedData( $data ) );
+
+		$content       = $templateRenderer->renderWithData( $template, $this->getMergedData( $data ) );
+		$projectConfig = $this->getProjectConfig();
+
+		$search  = array_keys( $projectConfig->getReplacements() );
+		$replace = array_values( $projectConfig->getReplacements() );
+		$content = str_replace( $search, $replace, $content );
+
+		echo $content;
+
 		flush();
 	}
 
+	private function getProjectConfig() : ProjectConfig
+	{
+		if ( null === $this->projectConfig )
+		{
+			$projectConfigFile = __DIR__ . '/../../../../../Project2018.json';
+			$projectConfigData = $this->getDataFromProjectConfig( $projectConfigFile );
+			$projectConfigDir  = \dirname( $projectConfigFile );
+
+			$this->projectConfig = new ProjectConfig( $projectConfigDir, $projectConfigData );
+		}
+
+		return $this->projectConfig;
+	}
+
+	private function getDataFromProjectConfig( string $projectConfigFile ) : array
+	{
+		$configData = file_get_contents( $projectConfigFile );
+
+		return json_decode( $configData, true );
+	}
+
+	/**
+	 * @param array $data
+	 *
+	 * @return array
+	 * @throws \PHPUGDD\PHPDD\Website\Tickets\Application\Exceptions\RuntimeException
+	 */
 	private function getMergedData( array $data ) : array
 	{
-		$projectConfig = $this->getDataFromProjectConfig();
-
-		$replacements = [];
-		foreach ( (array)$projectConfig['replacements'] as $key => $value )
-		{
-			$replacements[ str_replace( '@', '', $key ) ] = $value;
-		}
+		$projectConfig = $this->getProjectConfig();
+		$page          = $projectConfig->getPageConfigForUri( '/tickets.html' );
 
 		return array_merge(
 			[
-				'projectName' => $projectConfig['name'],
-				'baseUrl'     => $projectConfig['baseUrl'],
+				'project' => $projectConfig,
+				'page'    => $page,
 			],
-			$replacements,
 			$data
 		);
-	}
-
-	private function getDataFromProjectConfig() : array
-	{
-		$configData = file_get_contents( __DIR__ . '/../../../../../Project2018.json' );
-
-		return json_decode( $configData, true );
 	}
 }
