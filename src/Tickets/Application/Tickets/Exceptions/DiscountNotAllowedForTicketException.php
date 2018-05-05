185935114e4f3505e82aad5e37b2200ece19a1b5
Holger Woltersdorf <hw@hollo.me>
2018-05-05 20:11:45 +0200

    Add exception for discounts not allowed for tickets

---
+++ src/Tickets/Application/Tickets/Exceptions/DiscountNotAllowedForTicketException.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace PHPUGDD\PHPDD\Website\Tickets\Application\Tickets\Exceptions;
+
+use PHPUGDD\PHPDD\Website\Tickets\Application\Exceptions\InvalidArgumentException;
+
+final class DiscountNotAllowedForTicketException extends InvalidArgumentException
+{
+
+}
