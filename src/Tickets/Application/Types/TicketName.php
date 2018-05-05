b97d8b3a67185fcc2ec991f4c474e6b4c1435525
Holger Woltersdorf <hw@hollo.me>
2018-05-05 15:04:19 +0200

    Add ticket name value constraint and tests

--- src/Tickets/Application/Types/TicketName.php
+++ src/Tickets/Application/Types/TicketName.php
@@ -6,6 +6,8 @@
 namespace PHPUGDD\PHPDD\Website\Tickets\Application\Types;
 
 use Fortuneglobe\Types\AbstractStringType;
+use PHPUGDD\PHPDD\Website\Tickets\Application\Exceptions\InvalidArgumentException;
+use function trim;
 
 /**
  * Class TicketName
@@ -13,8 +15,16 @@ use Fortuneglobe\Types\AbstractStringType;
  */
 final class TicketName extends AbstractStringType
 {
+	/**
+	 * @param string $value
+	 *
+	 * @throws InvalidArgumentException
+	 */
 	protected function guardValueIsValid( string $value ) : void
 	{
-		// TODO: Implement guardValueIsValid() method.
+		if ( '' === trim( $value ) )
+		{
+			throw new InvalidArgumentException( 'Ticket name cannot be empty.' );
+		}
 	}
 }
---
+++ tests/Tickets/Unit/Application/Types/TicketNameTest.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace PHPUGDD\PHPDD\Website\Tests\Tickets\Unit\Application\Types;
+
+use PHPUGDD\PHPDD\Website\Tests\Tickets\Fixtures\Traits\EmptyStringProviding;
+use PHPUGDD\PHPDD\Website\Tickets\Application\Exceptions\InvalidArgumentException;
+use PHPUGDD\PHPDD\Website\Tickets\Application\Types\TicketName;
+use PHPUnit\Framework\TestCase;
+
+final class TicketNameTest extends TestCase
+{
+	use EmptyStringProviding;
+
+	/**
+	 * @param string $value
+	 *
+	 * @dataProvider emptyStringProvider
+	 */
+	public function testThrowsExceptionWhenConstructedWithEmptyString( string $value ) : void
+	{
+		$this->expectException( InvalidArgumentException::class );
+		$this->expectExceptionMessage( 'Ticket name cannot be empty.' );
+
+		new TicketName( $value );
+	}
+}
