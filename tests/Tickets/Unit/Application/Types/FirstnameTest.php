b7135889f46ad893221d844a17ad5ae6bdfb3a10
Holger Woltersdorf <hw@hollo.me>
2018-05-05 14:57:32 +0200

    Add firstname value constraint and tests

---
+++ tests/Tickets/Unit/Application/Types/FirstnameTest.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace PHPUGDD\PHPDD\Website\Tests\Tickets\Unit\Application\Types;
+
+use PHPUGDD\PHPDD\Website\Tests\Tickets\Fixtures\Traits\EmptyStringProviding;
+use PHPUGDD\PHPDD\Website\Tickets\Application\Exceptions\InvalidArgumentException;
+use PHPUGDD\PHPDD\Website\Tickets\Application\Types\Firstname;
+use PHPUnit\Framework\TestCase;
+
+final class FirstnameTest extends TestCase
+{
+	use EmptyStringProviding;
+
+	/**
+	 * @param string $value
+	 *
+	 * @dataProvider emptyStringProvider
+	 */
+	public function testThrowsExceptionWhenConstructedWithEmptyString( string $value ) : void
+	{
+		$this->expectException( InvalidArgumentException::class );
+		$this->expectExceptionMessage( 'Firstname cannot be empty.' );
+
+		new Firstname( $value );
+	}
+}
