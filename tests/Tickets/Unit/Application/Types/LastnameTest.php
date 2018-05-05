89666c85ca2d8d694e88357c9b964a213f029a44
Holger Woltersdorf <hw@hollo.me>
2018-05-05 14:57:42 +0200

    Add lastname value constraint and tests

---
+++ tests/Tickets/Unit/Application/Types/LastnameTest.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace PHPUGDD\PHPDD\Website\Tests\Tickets\Unit\Application\Types;
+
+use PHPUGDD\PHPDD\Website\Tests\Tickets\Fixtures\Traits\EmptyStringProviding;
+use PHPUGDD\PHPDD\Website\Tickets\Application\Exceptions\InvalidArgumentException;
+use PHPUGDD\PHPDD\Website\Tickets\Application\Types\Lastname;
+use PHPUnit\Framework\TestCase;
+
+final class LastnameTest extends TestCase
+{
+	use EmptyStringProviding;
+
+	/**
+	 * @param string $value
+	 *
+	 * @dataProvider emptyStringProvider
+	 */
+	public function testThrowsExceptionWhenConstructedWithEmptyString( string $value ) : void
+	{
+		$this->expectException( InvalidArgumentException::class );
+		$this->expectExceptionMessage( 'Lastname cannot be empty.' );
+
+		new Lastname( $value );
+	}
+}
