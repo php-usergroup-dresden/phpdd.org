2d29cd15d31602eafa7f2d0ab3478241b11ec4cd
Holger Woltersdorf <hw@hollo.me>
2018-05-22 22:20:36 +0200

    Finalize ticket order billing address tests

--- tests/Tickets/Unit/Application/Tickets/TicketOrderBillingAddressTest.php
+++ tests/Tickets/Unit/Application/Tickets/TicketOrderBillingAddressTest.php
@@ -11,6 +11,7 @@ use PHPUGDD\PHPDD\Website\Tickets\Application\Types\CountryCode;
 use PHPUGDD\PHPDD\Website\Tickets\Application\Types\Firstname;
 use PHPUGDD\PHPDD\Website\Tickets\Application\Types\Lastname;
 use PHPUGDD\PHPDD\Website\Tickets\Application\Types\StreetWithNumber;
+use PHPUGDD\PHPDD\Website\Tickets\Application\Types\VatNumber;
 use PHPUGDD\PHPDD\Website\Tickets\Application\Types\ZipCode;
 use PHPUnit\Framework\TestCase;
 
@@ -37,5 +38,34 @@ final class TicketOrderBillingAddressTest extends TestCase
 		$expectedString = "ACME Inc.\nJohn Doe\nACME Plaza 123\nc/o Cats & Dogs\nUS-98765 Uptown Hollywood";
 
 		$this->assertSame( $expectedString, $address->toString() );
+	}
+
+	/**
+	 * @throws \PHPUnit\Framework\ExpectationFailedException
+	 * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
+	 */
+	public function testCanGetAddressValues() : void
+	{
+		$address = new TicketOrderBillingAddress(
+			new Firstname( 'John' ),
+			new Lastname( 'Doe' ),
+			new CompanyName( 'ACME Inc.' ),
+			new StreetWithNumber( 'ACME Plaza 123' ),
+			new AddressAddon( 'c/o Cats & Dogs' ),
+			new ZipCode( '98765' ),
+			new City( 'Uptown Hollywood' ),
+			new CountryCode( CountryCodes::US_SHORT ),
+			new VatNumber( 'DE308538781' )
+		);
+
+		$this->assertSame( 'John', $address->getFirstname()->toString() );
+		$this->assertSame( 'Doe', $address->getLastname()->toString() );
+		$this->assertSame( 'ACME Inc.', $address->getCompanyName()->toString() );
+		$this->assertSame( 'ACME Plaza 123', $address->getStreetWithNumber()->toString() );
+		$this->assertSame( 'c/o Cats & Dogs', $address->getAddressAddon()->toString() );
+		$this->assertSame( '98765', $address->getZipCode()->toString() );
+		$this->assertSame( 'Uptown Hollywood', $address->getCity()->toString() );
+		$this->assertSame( CountryCodes::US_SHORT, $address->getCountryCode()->toString() );
+		$this->assertSame( 'DE308538781', $address->getVatNumber()->toString() );
 	}
 }
