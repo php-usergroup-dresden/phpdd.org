dfe1818e224db10e0d15d10a8cdd66eaa4d4c03b
Holger Woltersdorf <hw@hollo.me>
2018-05-26 21:58:05 +0200

    Disable error output

--- config/Sentry.php
+++ config/Sentry.php
@@ -2,7 +2,7 @@
 
 return [
 	'dsn'            => 'https://ac501ad7990e424fa9cf9f8a519443b4:aa5c99ddaa094a96bdd358a7cf5d1fde@sentry.io/267711',
-	'displayErrors'  => true,
+	'displayErrors'  => false,
 	'errorReporting' => E_ALL,
 	'environment'    => 'dev',
 	'release'        => 'v0.1.0-dev',
