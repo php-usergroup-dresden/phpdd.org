657b3146cda436e04964012c0d5020115513dc7f
Holger Woltersdorf <hw@hollo.me>
2018-05-27 01:50:28 +0200

    Add getter for ticket select form

--- src/Tickets/Infrastructure/Session.php
+++ src/Tickets/Infrastructure/Session.php
@@ -1,17 +1,22 @@
 <?php declare(strict_types=1);
-/**
- * @author hollodotme
- */
 
 namespace PHPUGDD\PHPDD\Website\Tickets\Infrastructure;
 
 use IceHawk\Bridges\SessionForms\AbstractSession;
+use IceHawk\Forms\Form;
+use IceHawk\Forms\FormId;
 
-/**
- * Class Session
- * @package PHPUGDD\PHPDD\Website\Tickets\Infrastructure
- */
 final class Session extends AbstractSession
 {
+	private const TICKET_SELECTION_FORM = 'ticketSelectionForm';
 
+	public function getTicketSelectionForm() : Form
+	{
+		return $this->getForm( new FormId( self::TICKET_SELECTION_FORM ) );
+	}
+
+	public function resetTicketOrderProcess() : void
+	{
+		$this->unsetForm( new FormId( self::TICKET_SELECTION_FORM ) );
+	}
 }
