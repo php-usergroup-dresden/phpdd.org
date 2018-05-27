cca49d496c6fd3f3d55d82cbca85ae3c250fa640
Holger Woltersdorf <hw@hollo.me>
2018-05-27 23:59:34 +0200

    Add save ticket details request handler with write route

--- config/WriteRoutes.php
+++ config/WriteRoutes.php
@@ -1,6 +1,7 @@
 <?php declare(strict_types=1);
 
 return [
-	'^/tickets/select/?$' => \PHPUGDD\PHPDD\Website\Tickets\Application\Web\Tickets\Write\SelectTicketsRequestHandler::class,
-	'^/tickets/abort/?$'  => \PHPUGDD\PHPDD\Website\Tickets\Application\Web\Tickets\Write\AbortTicketOrderRequestHandler::class,
+	'^/tickets/select/?$'       => \PHPUGDD\PHPDD\Website\Tickets\Application\Web\Tickets\Write\SelectTicketsRequestHandler::class,
+	'^/tickets/abort/?$'        => \PHPUGDD\PHPDD\Website\Tickets\Application\Web\Tickets\Write\AbortTicketOrderRequestHandler::class,
+	'^/tickets/save-details/?$' => \PHPUGDD\PHPDD\Website\Tickets\Application\Web\Tickets\Write\SaveTicketDetailsRequestHandler::class,
 ];
---
+++ src/Tickets/Application/Web/Tickets/Write/SaveTicketDetailsRequestHandler.php
@@ -0,0 +1,111 @@
+<?php declare(strict_types=1);
+
+namespace PHPUGDD\PHPDD\Website\Tickets\Application\Web\Tickets\Write;
+
+use IceHawk\Forms\Feedback;
+use IceHawk\Forms\Form;
+use IceHawk\IceHawk\Interfaces\HandlesPostRequest;
+use IceHawk\IceHawk\Interfaces\ProvidesWriteRequestData;
+use IceHawk\IceHawk\Interfaces\ProvidesWriteRequestInputData;
+use PHPUGDD\PHPDD\Website\Tickets\Application\Bridges\UserInput;
+use PHPUGDD\PHPDD\Website\Tickets\Application\Configs\TicketsConfig;
+use PHPUGDD\PHPDD\Website\Tickets\Application\Tickets\SelectedTicketInfos;
+use PHPUGDD\PHPDD\Website\Tickets\Application\Web\AbstractRequestHandler;
+use PHPUGDD\PHPDD\Website\Tickets\Application\Web\Responses\Redirect;
+use PHPUGDD\PHPDD\Website\Tickets\Application\Web\Tickets\Write\Interfaces\ValidatesUserInput;
+use PHPUGDD\PHPDD\Website\Tickets\Application\Web\Tickets\Write\Validators\AttendeeValidator;
+use PHPUGDD\PHPDD\Website\Tickets\Application\Web\Tickets\Write\Validators\BillingInformationValidator;
+use PHPUGDD\PHPDD\Website\Tickets\Application\Web\Tickets\Write\Validators\CompositeValidator;
+use PHPUGDD\PHPDD\Website\Tickets\Application\Web\Tickets\Write\Validators\DiscountValidator;
+use PHPUGDD\PHPDD\Website\Tickets\Application\Web\Traits\CsrfTokenChecking;
+use function implode;
+
+final class SaveTicketDetailsRequestHandler extends AbstractRequestHandler implements HandlesPostRequest
+{
+	use CsrfTokenChecking;
+
+	private const FAIL_URL              = '/tickets/details/';
+
+	private const SUCESS_URL            = '/tickets/payment/';
+
+	private const GENERAL_ERROR_MESSAGE = 'Your data is not valid yet. Please check the red error messages.';
+
+	public function handle( ProvidesWriteRequestData $request )
+	{
+		$input               = $request->getInput();
+		$session             = $this->getEnv()->getSession();
+		$ticketSelectionForm = $session->getTicketSelectionForm();
+		$ticketDetailsForm   = $session->getTicketDetailsForm();
+		$errorHandler        = $this->getEnv()->getErrorHandler();
+
+		$ticketDetailsForm->resetFeedbacks();
+		$ticketDetailsForm->setData( $input->getData() );
+
+		if ( $this->csrfCheckFailed( $ticketDetailsForm, (string)$input->get( 'token' ), $errorHandler ) )
+		{
+			$ticketDetailsForm->addFeedback( 'general', new Feedback( 'Invalid request. Please try again.' ) );
+
+			(new Redirect())->respond( self::FAIL_URL );
+
+			return;
+		}
+
+		$ticketsConfig       = TicketsConfig::fromConfigFile();
+		$selectedTickets     = (array)$ticketSelectionForm->get( 'selectedTickets' );
+		$selectedTicketInfos = new SelectedTicketInfos( $ticketsConfig, $selectedTickets );
+
+		$userInputValidator = $this->getUserInputValidator( $selectedTicketInfos, $input );
+
+		if ( $userInputValidator->failed() )
+		{
+			$ticketDetailsForm->addFeedback( 'general', new Feedback( self::GENERAL_ERROR_MESSAGE ) );
+			$this->addValidatorMessagesToForm( $ticketDetailsForm, $userInputValidator );
+
+			(new Redirect())->respond( self::FAIL_URL );
+
+			return;
+		}
+
+		(new Redirect())->respond( self::SUCESS_URL );
+	}
+
+	private function getUserInputValidator(
+		SelectedTicketInfos $selectedTicketInfos,
+		ProvidesWriteRequestInputData $input
+	) : ValidatesUserInput
+	{
+		$userInputValidator = new CompositeValidator();
+		$userInputValidator->add( new BillingInformationValidator( new UserInput( $input->getData() ) ) );
+
+		$attendees = (array)$input->get( 'attendees', [] );
+		$discounts = (array)$input->get( 'discounts', [] );
+
+		foreach ( $selectedTicketInfos->getTickets() as $selectedTicketInfo )
+		{
+			for ( $i = 0; $i < $selectedTicketInfo->getQuantity(); $i++ )
+			{
+				$ticketId = $selectedTicketInfo->getId()->toString();
+
+				$attendeeName      = $attendees[ $ticketId ][ $i ] ?? '';
+				$attendeeUserInput = new UserInput( ['attendeeName' => $attendeeName] );
+
+				$userInputValidator->add( new AttendeeValidator( $attendeeUserInput, $ticketId, $i ) );
+
+				$discountCode      = $discounts[ $ticketId ][ $i ] ?? '';
+				$discountUserInput = new UserInput( ['discountCode' => $discountCode] );
+
+				$userInputValidator->add( new DiscountValidator( $discountUserInput, $ticketId, $i ) );
+			}
+		}
+
+		return $userInputValidator;
+	}
+
+	private function addValidatorMessagesToForm( Form $form, ValidatesUserInput $validator ) : void
+	{
+		foreach ( $validator->getMessages() as $groupKey => $messages )
+		{
+			$form->addFeedback( $groupKey, new Feedback( implode( ' ', $messages ) ) );
+		}
+	}
+}
