6063c08446887d05891b930a5aa668237fc0525b
Holger Woltersdorf <hw@hollo.me>
2018-05-27 12:46:40 +0200

    Add abort ticket order request handler and write route

--- config/WriteRoutes.php
+++ config/WriteRoutes.php
@@ -2,4 +2,5 @@
 
 return [
 	'^/tickets/select/?$' => \PHPUGDD\PHPDD\Website\Tickets\Application\Web\Tickets\Write\SelectTicketsRequestHandler::class,
+	'^/tickets/abort/?$'  => \PHPUGDD\PHPDD\Website\Tickets\Application\Web\Tickets\Write\AbortTicketOrderRequestHandler::class,
 ];
---
+++ src/Tickets/Application/Web/Tickets/Write/AbortTicketOrderRequestHandler.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+
+namespace PHPUGDD\PHPDD\Website\Tickets\Application\Web\Tickets\Write;
+
+use IceHawk\IceHawk\Interfaces\HandlesPostRequest;
+use IceHawk\IceHawk\Interfaces\ProvidesWriteRequestData;
+use PHPUGDD\PHPDD\Website\Tickets\Application\Web\AbstractRequestHandler;
+use PHPUGDD\PHPDD\Website\Tickets\Application\Web\Responses\Redirect;
+
+final class AbortTicketOrderRequestHandler extends AbstractRequestHandler implements HandlesPostRequest
+{
+	private const SUCESS_URL = '/tickets/';
+
+	public function handle( ProvidesWriteRequestData $request )
+	{
+		$session = $this->getEnv()->getSession();
+		$session->abortTicketOrder();
+
+		(new Redirect())->respond( self::SUCESS_URL );
+	}
+}
