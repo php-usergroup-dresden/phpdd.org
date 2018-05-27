461fbba232c2c382011e64dd8dbb7b9f7756d928
Holger Woltersdorf <hw@hollo.me>
2018-05-27 15:12:55 +0200

    Finish layout of ticket details view

--- src/Tickets/Application/Web/Tickets/Read/TicketDetailsRequestHandler.php
+++ src/Tickets/Application/Web/Tickets/Read/TicketDetailsRequestHandler.php
@@ -4,9 +4,13 @@ namespace PHPUGDD\PHPDD\Website\Tickets\Application\Web\Tickets\Read;
 
 use IceHawk\IceHawk\Interfaces\HandlesGetRequest;
 use IceHawk\IceHawk\Interfaces\ProvidesReadRequestData;
+use PHPUGDD\PHPDD\Website\Tickets\Application\Configs\TicketsConfig;
+use PHPUGDD\PHPDD\Website\Tickets\Application\Constants\CountryCodes;
 use PHPUGDD\PHPDD\Website\Tickets\Application\Exceptions\RuntimeException;
+use PHPUGDD\PHPDD\Website\Tickets\Application\Tickets\SelectedTicketInfos;
 use PHPUGDD\PHPDD\Website\Tickets\Application\Web\AbstractRequestHandler;
 use PHPUGDD\PHPDD\Website\Tickets\Application\Web\Responses\HtmlPage;
+use function array_combine;
 
 final class TicketDetailsRequestHandler extends AbstractRequestHandler implements HandlesGetRequest
 {
@@ -19,13 +23,17 @@ final class TicketDetailsRequestHandler extends AbstractRequestHandler implement
 	{
 		$session           = $this->getEnv()->getSession();
 		$ticketSelectForm  = $session->getTicketSelectionForm();
+		$ticketsConfig     = TicketsConfig::fromConfigFile();
 		$selectedTickets   = $ticketSelectForm->get( 'selectedTickets' );
 		$ticketDetailsForm = $session->getTicketDetailsForm();
 		$ticketDetailsForm->renewToken();
 
+		$selectedTicketInfos = new SelectedTicketInfos( $ticketsConfig, $selectedTickets );
+
 		$data = [
-			'selectedTickets'   => $selectedTickets,
+			'selectedTickets'   => $selectedTicketInfos,
 			'ticketDetailsForm' => $ticketDetailsForm,
+			'countryCodes'      => array_combine( CountryCodes::ALL_SHORT, CountryCodes::ALL_LONG ),
 		];
 
 		(new HtmlPage( $this->getEnv() ))->respond( 'Tickets/Read/Pages/Details.twig', $data );
