600482c66608ee40521fca2d4ee4d4fe29784ac4
Holger Woltersdorf <hw@hollo.me>
2018-05-31 15:26:43 +0200

    Add payment radio selection

--- public/2018/assets/js/main.js
+++ public/2018/assets/js/main.js
@@ -2,4 +2,10 @@ $(document).ready(function () {
     $('[data-toggle=offcanvas]').click(function () {
         $('.row-offcanvas').toggleClass('active');
     });
+    const paymentMethods = $('.payment-methods');
+    paymentMethods.find('input[type="radio"]').change(function(e) {
+        const radio = $(this);
+        paymentMethods.find('div.radio').removeClass('active');
+        radio.parents('div.radio').addClass('active');
+    });
 });
