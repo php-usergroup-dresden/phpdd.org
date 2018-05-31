0a556fa3178e6ac10542636475d0f3929eed42d8
Holger Woltersdorf <hw@hollo.me>
2018-06-01 01:13:42 +0200

    Update paypal config

--- config/Paypal.php
+++ config/Paypal.php
@@ -4,8 +4,8 @@ return [
 	'authEndpoint' => 'sandbox',
 	'clientId'     => 'AbInKqyHMp9KeyMVSGs0y7U3Tn6hae7YS-xeidHiyYJBpv8SLEYeMvjsvkBTlUuX4lXavTr0tUOznYIP',
 	'clientSecret' => 'ECFmrw-G68mTRrBmC8kKXDle-kgYxzF6OdRi64KoBoNcVZ1D89MrSIbjrN7GrE2GcoNHDpfOPZJCQeug',
-	'redirectUrl'  => 'http://127.0.0.1:8080/tickets/',
+	'redirectUrl'  => 'https://2018.phpdd.dev/tickets/',
 	'scopes'       => 'profile email address phone https://uri.paypal.com/services/paypalattributes',
-	'cancelUrl'    => 'http://127.0.0.1:8080/tickets/paypal-canceled',
-	'successUrl'   => 'http://127.0.0.1:8080/tickets/paypal-success',
+	'cancelUrl'    => 'https://2018.phpdd.dev/tickets/paypal-canceled',
+	'successUrl'   => 'https://2018.phpdd.dev/tickets/paypal-success',
 ];
