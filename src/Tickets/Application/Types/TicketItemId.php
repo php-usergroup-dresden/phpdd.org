f7f7328c9ff161a09dd317a16533cb876257b8d8
Holger Woltersdorf <hw@hollo.me>
2018-06-01 13:59:51 +0200

    Add UUID4 type for ticket item IDs

---
+++ src/Tickets/Application/Types/TicketItemId.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+namespace PHPUGDD\PHPDD\Website\Tickets\Application\Types;
+
+use Fortuneglobe\Types\AbstractUuid4Type;
+use Fortuneglobe\Types\Interfaces\RepresentsUuid4Value;
+
+final class TicketItemId extends AbstractUuid4Type
+{
+
+}
