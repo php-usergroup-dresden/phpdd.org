b1f56f7c169f8bc7dead846e517766b437803ead
Holger Woltersdorf <hw@hollo.me>
2018-06-01 22:56:23 +0200

    Disable stripe button too if CoC checkbox is not ticked

--- public/2018/assets/js/main.js
+++ public/2018/assets/js/main.js
@@ -3,15 +3,18 @@ $(document).ready(function () {
         $('.row-offcanvas').toggleClass('active');
     });
     const paymentMethods = $('.payment-methods');
-    paymentMethods.find('input[type="radio"]').change(function(e) {
+    paymentMethods.find('input[type="radio"]').change(function (e) {
         const radio = $(this);
         paymentMethods.find('div.radio').removeClass('active');
         radio.parents('div.radio').addClass('active');
     });
     const cocOptIn = $('#cocOptIn');
     const buttonPurchase = $('#buttonPurchase');
-    cocOptIn.change(function() {
+    const stripeButton = $('button.stripe-button-el');
+    cocOptIn.change(function () {
         const checked = $(this).prop('checked');
         buttonPurchase.prop('disabled', !checked);
-    })
+        stripeButton.prop('disabled', !checked);
+    });
+    stripeButton.prop('disabled', true);
 });
