8f91fdc55d4e2a482bdfe10a81b32a131d792543
Holger Woltersdorf <hw@hollo.me>
2018-06-02 21:52:57 +0200

    Add search paths to twig config

--- config/Twig.php
+++ config/Twig.php
@@ -4,6 +4,8 @@ return [
 	'searchPaths'  => [
 		__DIR__ . '/../src/Contents/2018/Templates',
 		__DIR__ . '/../src/Tickets/Application/Web',
+		__DIR__ . '/../src/Tickets/Application/Invoices/Templates',
+		__DIR__ . '/../src/Tickets/Application/Tickets/Templates',
 	],
 	'debugEnabled' => true,
 	'cacheDir'     => '/var/tmp',
