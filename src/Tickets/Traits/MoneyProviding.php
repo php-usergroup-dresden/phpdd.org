c94adf874ffae23c0eb425e8b8fa1792fe08e4e1
Holger Woltersdorf <hw@hollo.me>
2018-06-02 21:52:12 +0200

    Add method to retrieve decimal formatted money

--- src/Tickets/Traits/MoneyProviding.php
+++ src/Tickets/Traits/MoneyProviding.php
@@ -5,7 +5,10 @@
 
 namespace PHPUGDD\PHPDD\Website\Tickets\Traits;
 
+use InvalidArgumentException;
+use Money\Currencies\ISOCurrencies;
 use Money\Currency;
+use Money\Formatter\DecimalMoneyFormatter;
 use Money\Money;
 
 /**
@@ -18,10 +21,23 @@ trait MoneyProviding
 	 * @param int $amount
 	 *
 	 * @return Money
-	 * @throws \InvalidArgumentException
+	 * @throws InvalidArgumentException
 	 */
 	protected function getMoney( int $amount ) : Money
 	{
 		return new Money( $amount, new Currency( 'EUR' ) );
+	}
+
+	/**
+	 * @param int $amount
+	 *
+	 * @throws InvalidArgumentException
+	 * @return string
+	 */
+	protected function getDecimalFormattedMoney( int $amount ) : string
+	{
+		$formatter = new DecimalMoneyFormatter( new ISOCurrencies() );
+
+		return $formatter->format( $this->getMoney( $amount ) );
 	}
 }
