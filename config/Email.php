559cc29f5500488e24292ab8e3160682596d0669
Holger Woltersdorf <hw@hollo.me>
2018-06-02 22:01:26 +0200

    Update deploy and email config

--- config/Email.php
+++ config/Email.php
@@ -4,7 +4,7 @@ return [
 	'smtpHost'      => 'mail.agenturserver.de',
 	'smtpPort'      => 587,
 	'smtpUser'      => 'p268406p6',
-	'smtpPassword'  => 'iwuTemog:651',
+	'smtpPassword'  => '',
 	'useTls'        => true,
 	'attachmentDir' => __DIR__ . '/../data/mail/attachments',
 	'sender'        => ['orga@phpug-dresden.org' => 'PHP USERGROUP DRESDEN e.V.'],
--- config/deploy.rb
+++ config/deploy.rb
@@ -17,18 +17,20 @@ set :keep_releases, 3
 # They will be linked in the 'deploy:link_shared_paths' step.
 set :shared_files, [
     "config/Discounts.php",
+    "config/Email.php",
     "config/MySql.php",
     "config/Paypal.php",
+    "config/phpbu.xml",
     "config/Sentry.php",
     "config/Session.php",
     "config/Stripe.php",
     "config/Twig.php",
-    "config/phpbu.xml"
 ]
 set :shared_dirs, [
     "public/2017/media",
     "public/2018/downloads",
-    "data/static"
+    "data/static/done"
+    "data/mail/attachments"
 ]
 
 # Optional settings:
